e1161707c035944ed4331793deae554860a466d1
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2020-10-10 04:50:39 +0900

    fixed beta formula with dirty solution

--- flatcam-beta.rb
+++ flatcam-beta.rb
@@ -2,140 +2,25 @@ class FlatcamBeta < Formula
     include Language::Python::Virtualenv
     desc "FlatCAM: 2D Computer-Aided PCB Manufacturing(Beta)"
     homepage "http://flatcam.org/"
-    url "https://bitbucket.org/jpcgt/flatcam.git", :branch=> "Beta",:revision => "900631b92e27ee96bb95964bdd2b723c0baf086c"
+    url "https://bitbucket.org/jpcgt/flatcam.git", :branch=> "Beta",:revision => "a58cd5d9473554b9a620eb5eb44fb766f26ef693"
     version "8.5.0"
     depends_on "pyqt5"
     depends_on "gdal"
     depends_on "freetype"
     depends_on "geos"
-    depends_on "pkg-config"
+    depends_on "pkg-config" => :build
     depends_on "python" => "with-tcl-tk"
     depends_on "spatialindex"
-    # pip tools "numpy","matplotlib" ,"rtree", "scipy", "shapely","simplejson" ,"svg.path"
-    resource "affine" do
-        url "https://files.pythonhosted.org/packages/58/84/0eb16b8223a665daa5c8e53ee4326d1f62dce9009e4de65aacafdabd533f/affine-2.2.2.tar.gz"
-        sha256 "ff0d0f40a90faa651f7bc7fece15bdbb7a0e0658b1e7ba6a03422c21efa7da90"
-      end
-    
-      resource "attrs" do
-        url "https://files.pythonhosted.org/packages/cc/d9/931a24cc5394f19383fbbe3e1147a0291276afa43a0dc3ed0d6cd9fda813/attrs-19.1.0.tar.gz"
-        sha256 "f0b870f674851ecbfbbbd364d6b5cbdff9dcedbc7f3f5e18a6891057f21fe399"
-      end
-    
-      resource "click" do
-        url "https://files.pythonhosted.org/packages/f8/5c/f60e9d8a1e77005f664b76ff8aeaee5bc05d0a91798afd7f53fc998dbc47/Click-7.0.tar.gz"
-        sha256 "5b94b49521f6456670fdb30cd82a4eca9412788a93fa6dd6df72c94d5a8ff2d7"
-      end
-    
-      resource "click-plugins" do
-        url "https://files.pythonhosted.org/packages/5f/1d/45434f64ed749540af821fd7e42b8e4d23ac04b1eda7c26613288d6cd8a8/click-plugins-1.1.1.tar.gz"
-        sha256 "46ab999744a9d831159c3411bb0c79346d94a444df9a3a3742e9ed63645f264b"
-      end
-    
-      resource "cligj" do
-        url "https://files.pythonhosted.org/packages/7a/75/20752146e9c73e74a286f66d4ee84ed102b4855c04d9bb8892b52a1f166c/cligj-0.5.0.tar.gz"
-        sha256 "6c7d52d529a78712491974f975c33473f430c0f7beb18c0d7a402a743dcb460a"
-      end
-    
-      resource "dill" do
-        url "https://files.pythonhosted.org/packages/fe/42/bfe2e0857bc284cbe6a011d93f2a9ad58a22cb894461b199ae72cfef0f29/dill-0.2.9.tar.gz"
-        sha256 "f6d6046f9f9195206063dd0415dff185ad593d6ee8b0e67f12597c0f4df4986f"
-      end
-    
-      resource "ezdxf" do
-        url "https://files.pythonhosted.org/packages/bc/41/fa64c527bde5f5a863c9f964d2ed686fd0a095bd5ccdb5247783650f01fc/ezdxf-0.9.zip"
-        sha256 "401cca861a04ffdc3a9ae46af6aa6c0ee6009cd5dfecf7220389ee866f8131bd"
-      end
-    
-      resource "fonttools" do
-        url "https://files.pythonhosted.org/packages/8b/2d/b6d2070306f5ecae50d22995eff1e0d041435851cd8ff41491bf28731285/fonttools-3.42.0.zip"
-        sha256 "c00b393b8fb8644acc7a0c7b71d2e70eadc21db494baaa05b32b08148c661670"
-      end
-    
-      resource "freetype-py" do
-        url "https://files.pythonhosted.org/packages/e3/0f/30965bd690e1f35dfd50aa856fac006327e402d1a36d91e4e343f08e7b12/freetype-py-2.1.0.post1.tar.gz"
-        sha256 "76383bb3e59efa6ce0be1797ed70207d7d1e421611df3aceb269673c4a77c2cc"
-      end
-    
-      resource "lxml" do
-        url "https://files.pythonhosted.org/packages/7d/29/174d70f303016c58bd790c6c86e6e86a9d18239fac314d55a9b7be501943/lxml-4.3.3.tar.gz"
-        sha256 "4a03dd682f8e35a10234904e0b9508d705ff98cf962c5851ed052e9340df3d90"
-      end
-    
-      resource "numpy" do
-        url "https://files.pythonhosted.org/packages/d3/4b/f9f4b96c0b1ba43d28a5bdc4b64f0b9d3fbcf31313a51bc766942866a7c7/numpy-1.16.4.zip"
-        sha256 "7242be12a58fec245ee9734e625964b97cf7e3f2f7d016603f9e56660ce479c7"
-      end
-    
-      resource "ortools" do
-        url "https://github.com/google/or-tools/archive/v7.1.tar.gz"
-        sha256 "6118a931ffcf172c7e2371c3392f82380e9d2a29840daba7caff239ad70d4b44"
-      end
-    
-      resource "protobuf" do
-        url "https://files.pythonhosted.org/packages/65/95/8fe278158433a9bc34723f9ecbdee3097fb6baefaca932ec0331a9f80244/protobuf-3.8.0.tar.gz"
-        sha256 "8c61cc8a76e9d381c665aecc5105fa0f1878cf7db8b5cd17202603bcb386d0fc"
-      end
-    
-      resource "PyOpenGL" do
-        url "https://files.pythonhosted.org/packages/9c/1d/4544708aaa89f26c97cc09450bb333a23724a320923e74d73e028b3560f9/PyOpenGL-3.1.0.tar.gz"
-        sha256 "9b47c5c3a094fa518ca88aeed35ae75834d53e4285512c61879f67a48c94ddaf"
-      end
-    
-      resource "PyOpenGL-accelerate" do
-        url "https://files.pythonhosted.org/packages/d9/74/293aa8794f2f236186d19e61c5548160bfe159c996ba01ed9144c89ee8ee/PyOpenGL-accelerate-3.1.0.tar.gz"
-        sha256 "927f4670b893d46e2f6273ae938bf0a1db27ffae3336eba94813ccef6260c410"
-      end
-    
-      resource "pyparsing" do
-        url "https://files.pythonhosted.org/packages/5d/3a/24d275393f493004aeb15a1beae2b4a3043526e8b692b65b4a9341450ebe/pyparsing-2.4.0.tar.gz"
-        sha256 "1873c03321fc118f4e9746baf201ff990ceb915f433f23b395f5580d1840cb2a"
-      end
-    
-      resource "rasterio" do
-        url "https://files.pythonhosted.org/packages/46/ee/f9b4bfb55b6cf1b5562e0b16bc75ab2ca3810587753e4c9944279d75540c/rasterio-1.0.23.tar.gz"
-        sha256 "45e1a4b3c5c58a0b9f1a6bfd0aa08bdc15da57fa690a1f6f26d1a6a3d3cc7aa5"
-      end
-    
-      resource "Rtree" do
-        url "https://files.pythonhosted.org/packages/b0/6c/6cc8d738f14d5efa0c38ec29403bbd9c75e64b3fe84b53290178dda0dbd9/Rtree-0.8.3.tar.gz"
-        sha256 "6cb9cf3000963ea6a3db777a597baee2bc55c4fc891e4f1967f262cc96148649"
-      end
-    
-      resource "Shapely" do
-        url "https://files.pythonhosted.org/packages/a2/fb/7a7af9ef7a35d16fa23b127abee272cfc483ca89029b73e92e93cdf36e6b/Shapely-1.6.4.post2.tar.gz"
-        sha256 "c4b87bb61fc3de59fc1f85e71a79b0c709dc68364d9584473697aad4aa13240f"
-      end
-    
-      resource "simplejson" do
-        url "https://files.pythonhosted.org/packages/e3/24/c35fb1c1c315fc0fffe61ea00d3f88e85469004713dab488dee4f35b0aff/simplejson-3.16.0.tar.gz"
-        sha256 "b1f329139ba647a9548aa05fb95d046b4a677643070dc2afc05fa2e975d09ca5"
-      end
-    
-      resource "six" do
-        url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
-        sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
-      end
-    
-      resource "snuggs" do
-        url "https://files.pythonhosted.org/packages/55/9c/2574e61fa5017d2128b39c45a32e65963b2aaea35c2d3a5b78a539699044/snuggs-1.4.6.tar.gz"
-        sha256 "8b87e5754fd2d0d1295b70bd502419f6f82be98c4bf668552da9d7c5f4387d1f"
-      end
-    
-      resource "svg.path" do
-        url "https://files.pythonhosted.org/packages/5a/7f/7a601000fc400024f76e660569b0b97f98787279daff079f0dbfa89293ba/svg.path-3.0.tar.gz"
-        sha256 "7b568f90f67fd25413c8da9f8bc9f9f8ab089425c20fa03330e97e77d13880ee"
-      end
-    
-      resource "vispy" do
-        url "https://files.pythonhosted.org/packages/5f/e2/1c9f4534a335f922bddf5fbd7ed1b105b91660b9ac32e21c39fa62544386/vispy-0.5.3.tar.gz"
-        sha256 "f6c36408df851b91322c928aadb2c8aaf3b31150e11f723784cc81b6280fdcc0"
-      end
-  
+
     def install
       venv = virtualenv_create(libexec,"python3")
-      venv.pip_install resources
-      venv.pip_install_and_link buildpath
+      inreplace "FlatCAM.py", "import sys", "#!#{libexec}/bin/python3\nimport sys"
+      system libexec/"bin/pip", "install", "descartes" #missing dependency
+      system libexec/"bin/pip", "install", "-r", "requirements.txt"
+      libexec.install Dir["*.py","appEditors","appGUI","appObjects","appParsers","appTools","assets","config","descrtes","doc","flatcamEditors","flatcamGUI","flatcamTools","lib","locale","locale_template","postprocessors","preprocessors","py2","tclCommands","Utils"]
+      bin.install_symlink libexec/"FlatCAM.py" => "flatcam"
+      system "chmod", "+x" , libexec/"FlatCAM.py"
+      system "chmod", "+x" , bin/"flatcam"
     end
     test do
   
